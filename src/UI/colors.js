57197701978ff4e11e4732f912cc264f31046db4
lucas Cincunegui <lucascincunegui@gmail.com>
2022-03-16 23:49:43 -0300

    base loja abba

---
+++ src/UI/colors.js
@@ -0,0 +1,5 @@
+export const green = "#728066";
+
+export const white = "#fff";
+
+export const lightGray = "#d7d7d7";
---
+++ src/screens/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { green, white, lightGray } from "../UI/colors";
+
+export const Container = styled.div`
+  background-color: ${lightGray};
+`;
+
+export const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: ${green};
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  color: ${white};
+  font-weight: bold;
+`;
+
+export const Parrafo = styled.p`
+  font-weight: bold;
+`;
+
+export const Image = styled.img`
+  width: 200px;
+  height: 200px;
+`;
